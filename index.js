f35564e8d008946bffe609780b3008c24fdb2f65
Rickzerr94 <rickzerr91@gmail.com>
2023-03-05 14:22:54 +0200

    lesson2 is done

---
+++ index.js
@@ -0,0 +1,13 @@
+// let admin, name;
+// name = 'Ivan';
+// admin = name;
+// alert(admin);
+
+// let myPlanetName = 'Earth'
+// let currentVisiter = 'Yarick';
+
+// let name = 'Illya';
+// alert(`hello ${name}`);
+
+let name = prompt('What is your name', '');
+alert(`Hello, ${name}! How are you?`);
