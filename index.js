ed633466d9466524a912bd2ae397618aba3c0649
Rickzerr94 <rickzerr91@gmail.com>
2023-03-16 18:33:02 +0200

    lesson5 is done + work under mistakes

--- index.js
+++ index.js
@@ -1,158 +1,43 @@
-
-const firstNumber = parseInt(prompt('enter first number')) || 0;
-
-const secondNumber = parseInt(prompt('enter second number')) || 22;
-
-let thirdNumber;
-    if(firstNumber && secondNumber){
-        thirdNumber = 80;
-    } else{
-        thirdNumber = 40;
-    };
-
-const emptyObject = {}
-    if (firstNumber >= 90){
-        emptyObject.size = 'big';
-    } else if(firstNumber <= 40){
-        emptyObject.size = 'small';
-    }else{
-        emptyObject.size = 'medium';
-    };
-
-let fourNumber;
-    switch (emptyObject.size){
-        case 'big':
-            fourNumber = 1000;
-            break;
-        case 'medium':
-            fourNumber = 100;
-            break;
-        case 'small':
-            fourNumber = 10;
-            break;
-    };
-
-const result = firstNumber*secondNumber*thirdNumber*fourNumber;
-    if (result % 2 !== 0){
-        console.log('Your never seen this');
-    } else{
-        if(secondNumber > 50){
-            alert(secondNumber);
-        } else{
-            alert('Not match to conditions');
-        };
-    };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const nameJs = prompt('What official name JavaScript?');
+// let fruits = ['apples','pear','banana'];
+// let shoppingCart = fruits;
+// shoppingCart.push('oranges');
+// console.log(fruits);
 //
-// if (nameJs === 'ECMAScript') {
-//     alert('Your dammit rigth!');
-// } else {
-//     alert('Are you fucking dont know ECMAScript??');
+// let styles = ['jazz','blues'];
+// alert(styles);
+// let addStyle = styles;
+// addStyle.push('rock-n-roll');
+// alert(addStyle);
+// let addStyleClassic = styles;
+// addStyleClassic[Math.floor((styles.length - 1) / 2)] = 'classic';
+// alert(addStyleClassic);
+// let shiftStyle = styles;
+// shiftStyle.shift();
+// alert(shiftStyle);
+// let unShiftStyle = styles;
+// unShiftStyle.unshift('rap', 'reggae');
+// alert(unShiftStyle);
+
+// let arr = ['a', 'b'];
+// arr.push(function () {
+//     alert(this);
+// });
+//
+// arr[2]();
+
+// function sumInput(){
+//     let numbers = [];
+//     while(true){
+//         let value = prompt('Enter the number');
+//         if(value === '' || value === null || !isFinite(value)) break;
+//         numbers.push(parseInt(value));
+//     };
+//     let sum = 0;
+//     for(let number of numbers){
+//         sum += number;
+//     };
+//     return sum;
 // };
-
-// const num = +prompt('enter hte number');
-//
-// if ( num > 0){
-//     alert('1');
-// } else if(num < 0){
-//     alert('-1');
-// } else if(num === 0){
-//     alert('0');
-// };
-
-// let result = a + b < 4 ? 'lower' : 'upper';
-
-// let message = (login == 'worker') ? 'hi' :
-//     (login == 'director') ? 'Hello' :
-//     (login == '') ? 'no login' :
-//     '';
+// alert( sumInput());
 
 
-// if(age >= 14 && age <= 90){
-//
-// }
-
-// if(!(age >= 14 && age <= 90)){
-//
-// };
-
-// const userName = prompt('Who is there?');
-//
-// if(userName === 'Admin'){
-//
-//     let pass = prompt('Hello, Admin! Please, enter your password!');
-//
-//     if(pass === "TheMaster"){
-//         alert('Welcome, my dungeon Master!');
-//     } else if(pass === '' || pass === null){
-//         alert('Canceled');
-//     } else{
-//         alert('Wrong password, motherfucker!!!');
-//     }
-//
-// } else if(userName === '' || userName === null){
-//     alert('Canceled');
-// } else{
-//     alert('I dont know you!');
-// };
-
-// if(browser === 'Edge'){
-//     alert( "you've got hge Edge!");
-// } else if(browser === 'Chrome'
-//     || browser === 'Firefox'
-//     || browser === 'Safari'
-//     || browser === 'Opera'){
-//     alert("we're support this browsers too");
-// } else{
-//     alert("We hope, this page looking good")
-// };
-
-// const a = +prompt('a?')
-//
-// switch (a){
-//     case 0:
-//         alert(0);
-//         break;
-//
-//     case 1:
-//         alert(1);
-//         break;
-//
-//     case 2:
-//     case 3:
-//         alert('2,3');
-//         break;
-// }  ;
