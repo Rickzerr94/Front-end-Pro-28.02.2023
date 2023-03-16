dd07c720989a83a055cdce785c84d4df791b2f30
Rickzerr94 <rickzerr91@gmail.com>
2023-03-16 18:48:10 +0200

    lesson5 is done + work under mistakes

--- index.js
+++ index.js
@@ -46,12 +46,26 @@ for(let i = 100; i <= 300; i++) {
 
 /////////
 
-const num4 = parseInt(prompt('Enter number!'));
+// const num4 = parseInt(prompt('Enter number!'));
+//
+// let item = 0;
+// for(let i = 2; i <= num4; i++) {
+//     if(num4 % i === 0){
+//         item += 1;
+//     };
+// };
+// console.log(item === 1) ? 'try number' : 'not a try number');
 
-let item = 0;
-for(let i = 2; i <= num4; i++) {
-    if(num4 % i === 0){
-        item += 1;
-    };
+const number = parseInt(prompt('Enter a positive number'));
+let isPrime = true;
+if(number === 1){
+    alert('Please, enter number biggest, than 1')
+} else if(number > 1){
+    for(let i =2; i < number; i++){
+        if(number % i === 0){
+            isPrime = false;
+            break;
+        }
+    }
+    isPrime ? alert(`${number} is a prime number`) : alert(`${number} is a not prime number`);
 };
-console.log(item === 1 ? 'try number' : 'not a try number');
