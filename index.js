225b52ba62bb7c76fa8a7cb75904978ec6c8bec5
Rickzerr94 <rickzerr91@gmail.com>
2023-03-28 19:00:12 +0300

    less9 done

--- index.js
+++ index.js
@@ -1,124 +1,46 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
+const arr = [[[[[[8]]]]]];
 
-function removeElement(array, item){
-    const index = array.indexOf(item);
-    if(index > -1){
-        array.splice(index, 1);
-    }
-}
-removeElement(array, 5);
-console.log(array);
+//this variant which i'l googled
 
-////
-
-const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-
-function generateKey(length, characters){
-    let result = '';
-    const  charactersLength = characters.length;
-    for(let i = 0; i < length; i++){
-        result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    return result;
-}
-
-const key = generateKey(16, characters);
-console.log(key);
-
-////
-
-function removeChars(inputString = "hello world", charsToRemove = ['l', 'd']){
-    let outputString = '';
-    for(let i = 0; i < inputString.length; i++){
-        let char = inputString[i];
-        if(!charsToRemove.includes(char)){
-            outputString += char;
-        }
-    }
-    return outputString;
-}
-console.log(removeChars());
-
-////
-
-function counterCreator(){
-    let value = 0;
-    function sumOfCounter(num){
-        return (value += num);
-    }
-    return sumOfCounter;
-}
-
-const counter = counterCreator()
-console.log(counter(3));
-console.log(counter(5));
-console.log(counter(20));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function checkAge(age) {
-//     return age > 18 ? true : confirm('Батьки дозволили?');
-    // if (age > 18) {
-    //     return true;
-    // } else {
-    //     return confirm('Батьки дозволили?');
-    // }
-// }
-
-// function min(a, b){
-//     return a < b ? a : b;
-// }
-// console.log(min(3, -1));
-
-// function pow(x, n){
-//     let result = x;
-//     for(let i = 1; i < n; i++){
-//         result *= x;
+// function factorialArray(arr){
+//     if(typeof arr === 'number'){
+//         if(arr <= 1){
+//             return 1
+//         }else{
+//             return arr * factorialArray(arr - 1);
+//         }
+//     }else{
+//         return factorialArray(arr[0]);
 //     }
-//     return result;
-// }
-// let x = prompt('x?');
-// let n = prompt('n?');
-//
-// if(n < 1){
-//     alert(`number ${n} is incorrect, use prime number`);
-// }else{
-//     alert(pow(x, n));
 // }
 
-// let ask = (question, yes, no) => confirm(question) ? yes() : no();
-// ask('Are you agree?', () => alert('You agree'), () => alert('You canceled'));
+//this variant from lection
 
+const factorialArray = function (arr){
+    if(arr === 1) return arr
+    return arr * factorialArray(arr - 1)
+}
+
+console.log(factorialArray(arr[0]));
+
+///////////////
+
+let calculator = {
+    a: 0,
+    b: 0,
+    read(){
+        this.a = parseInt(prompt('enter first number'));
+        this.b = parseInt(prompt('enter second number'));
+    },
+    sum(){
+        return this.a + this.b;
+    },
+    mul(){
+        return this.a * this.b;
+    }
+};
+
+calculator.read();
+alert(calculator.sum());
+alert(calculator.mul());
 
