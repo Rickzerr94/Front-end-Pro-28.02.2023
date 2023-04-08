72ae01a87509d1c610a1e44336322e8ad4775d42
Rickzerr94 <rickzerr91@gmail.com>
2023-04-08 22:56:35 +0300

    less11 done

--- index.js
+++ index.js
@@ -1,140 +1,53 @@
-const arr = [1,2,3,4,5,1,2,3,4,5]
+function  solution(arr) {
+    const freq = {};
 
-const yourFunc = (arr) => {
-    return Array.from(new Set(arr));
-};
-
-yourFunc(arr);
-
-////
-
-function mostFrequent(arr) {
-    let count = {};
-
-    for (let i = 0; i < arr.length; i++) {
-        let elem = arr[i];
-        if (elem in count) {
-            count[elem]++;
-        } else {
-            count[elem] = 1;
-        }
+    for (let num of arr) {
+        freq [num] = (freq[num] || 0) +1;
     }
 
-    let maxCount = 0;
-    let mostCommon;
-    for (let elem in count) {
-        if (count[elem] > maxCount) {
-            maxCount = count[elem];
-            mostCommon = elem;
+    const result = [];
+
+    for (let key in freq) {
+        if (freq[key] % 2 !== 0) {
+            result.push(parseInt(key));
         }
     }
-
-    return mostCommon;
+    return result;
 }
 
-console.log(mostFrequent(arr))
+console.log(solution([12, 23, 34, 12, 12, 23, 12, 45]));
+console.log(solution([4, 4, 100, 5000, 4, 4, 4, 4, 4, 100, 100]));
+console.log(solution([3, 3, 4, 6, 5, 4, 9, 9, 21, 9]));
+console.log(solution([4, 8, 15, 16, 23, 42, 4, 15, 42, 42]));
+console.log(solution([2, 2, 44, 44]));
 
 ////
 
-//Буде undefined, бо останній рядок викличе функцію z з аргументом x яка є сама функція без параметрів яка ж повертає
-//значення змінної y
-//через те що в подальшому викликаєтся аргумент t, котрий сам є функцією без параметрів, то на цбому ланцюжок завершиться і нічого
-//не виведеться, а це значить що буде undefined
+function ezjQuery(tag) {
+    let htmlString = `<${tag}></${tag}>`;
 
-////
+    function add(tag, text = '') {
+        htmlString = htmlString.replace(`</${tag}>`, `<${tag}>${text}</${tag}>`);
+        return this;
+    }
 
-function debounce(f, ms) {
-    let isCooldown = false;
+    function render() {
+        return htmlString;
+    }
 
-    return function () {
-        if (isCooldown) return;
-        f.apply(this, arguments);
-        isCooldown = true;
-
-        setTimeout(() => isCooldown = false, ms);
-    };
+    return { add, render };
 }
 
-let f = debounce(alert, 1000)
+let helloList = ezjQuery('body')
+    .add('div')
+    .add('ul')
+    .add('li', 'Hello')
+    .render();
 
-f(1);
-f(2);
+console.log(helloList);
 
-setTimeout(() => f(3), 100);
-setTimeout(() => f(4), 1100);
-setTimeout(() => f(5), 1500);
+let bodyDiv = ezjQuery('body')
+    .add('div')
+    .render();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function unique(arr) {
-//     return Array.from(new Set(arr));
-// }
-//
-// let values = ["Hare", "Krishna", "Hare", "Krishna",
-//     "Krishna", "Krishna", "Hare", "Hare", ":-O"
-// ];
-//
-// alert( unique(values) );
-
-// function Accumulator(startingValue) {
-//     this.value = startingValue;
-//
-//     this.read = function () {
-//         this.value += parseInt(prompt('how to count?'));
-//     }
-// }
-//
-// let accumulator = new Accumulator(1);
-// accumulator.read();
-// accumulator.read();
-//
-// alert(accumulator.value);
-
-// function printNumbers(from, to){
-//     let current = from;
-//
-//     let timerId = setInterval(function () {
-//         alert(current);
-//         if(current === to) {
-//             clearInterval(timerId);
-//         }
-//         current++;
-//     }, 1000);
-// }
-//
-// printNumbers(5, 12);
-
-// function printNumbers(from, to) {
-//     let current = from;
-//
-//     setTimeout(function go() {
-//         alert(current);
-//         if(current < to) {
-//             setTimeout(go, 1000)
-//         }
-//         current++
-//     }, 1000)
-// }
-//
-// printNumbers(5, 11);
+console.log(bodyDiv)
