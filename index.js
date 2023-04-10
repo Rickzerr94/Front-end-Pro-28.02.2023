6547d9c3282a928dc2a828784556b19ae1355dda
Rickzerr94 <rickzerr91@gmail.com>
2023-04-10 15:53:21 +0300

    less12

--- index.js
+++ index.js
@@ -17,3 +17,57 @@ console.log(solution("0123456789abcdefghijklmnop"));
 
 ////
 
+function solutionWorld(sentence){
+    sentence = sentence.replace(/,/g, '').replace(/!/g, '').replace(/\.{3}/g, '');
+
+    let words = sentence.split(' ');
+    let averageLength = words.reduce((acc, cur) => acc + cur.length, 0) / words.length;
+    let result = words.filter(word => word.length > averageLength);
+    return result;
+}
+
+console.log(solutionWorld("This is a sample string"));
+console.log(solutionWorld("Some another sample"));
+console.log(solutionWorld("Do, do, do, do... do it!"));
+
+////
+
+const database = {
+    adminPassword: 'password',
+    users: []
+};
+
+const user = {
+    email: '',
+    getAdminEmail: function () {
+        return 'admin@example.com'
+    }
+};
+
+const verifiedUser = Object.create(user);
+verifiedUser.isVerified = true;
+
+const guest = Object.create(user);
+guest.register = function () {
+    database.users.push(this);
+};
+
+const databaseObject = Object.create(guest);
+databaseObject.adminPassword = database.adminPassword;
+databaseObject.getAdminEmail = user.getAdminEmail;
+databaseObject.getUsers = function () {
+    return database.users;
+};
+
+const user1 = Object.create(user);
+user1.email = "user1@example.com";
+
+const verifiedUser1 = Object.create(verifiedUser);
+verifiedUser1.email = "verifieduser1@example.com";
+
+const guest1 = Object.create(guest);
+guest1.email = "guest1@example.com";
+
+guest1.register();
+
+const databaseUsers = databaseObject.getUsers();
