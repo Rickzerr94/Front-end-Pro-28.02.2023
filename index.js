fff4291af034a2401702157c14c48bbec1dbf910
Rickzerr94 <rickzerr91@gmail.com>
2023-04-10 15:08:53 +0300

    less12

--- index.js
+++ index.js
@@ -1,56 +1,19 @@
-function  solution(arr) {
-    const freq = {};
-
-    for (let num of arr) {
-        freq [num] = (freq[num] || 0) +1;
-    }
-
-    const result = [];
-
-    for (let key in freq) {
-        if (freq[key] % 2 !== 0) {
-            result.push(parseInt(key));
+function solution(str){
+    let alphabet = 'abcdefghijklmnopqrstuvwxyz';
+    for (let i = 0; i <alphabet.length; i++){
+        if(str.indexOf(alphabet[i]) === -1){
+            return false;
         }
     }
-    return result;
+    return true;
 }
 
-console.log(solution([12, 23, 34, 12, 12, 23, 12, 45]));
-console.log(solution([4, 4, 100, 5000, 4, 4, 4, 4, 4, 100, 100]));
-console.log(solution([3, 3, 4, 6, 5, 4, 9, 9, 21, 9]));
-console.log(solution([4, 8, 15, 16, 23, 42, 4, 15, 42, 42]));
-console.log(solution([2, 2, 44, 44]));
+console.log(solution("wyyga"));
+console.log(solution("qwertyuioplkjhgfdsazxcvbnm"));
+console.log(solution("ejuxggfsts"));
+console.log(solution("qpwoeirutyalskdjfhgmznxbcv"));
+console.log(solution("qqqqqqqqpwoeirutyalskdjfhgmznxbcv"));
+console.log(solution("0123456789abcdefghijklmnop"));
 
 ////
 
-function ezjQuery(tag) {
-    let htmlString = `<${tag}></${tag}>`;
-
-    function add(tagName, text = '') {
-        const newHtmlString = `<${tagName}>${text}</${tagName}>`
-        htmlString = htmlString.replace(`</${tag}>`, `${newHtmlString}</${tag}>`);
-        return this;
-    }
-
-    function render() {
-        return htmlString;
-    }
-
-    return { add, render };
-}
-
-
-
-let helloList = ezjQuery('body')
-    .add('div')
-    .add('ul')
-    .add('li', 'Hello')
-    .render();
-
-console.log(helloList);
-
-let bodyDiv = ezjQuery('body')
-    .add('div')
-    .render();
-
-console.log(bodyDiv)
