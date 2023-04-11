69ee26e546858de7f4b489a33d31ad936065e4ba
Rickzerr94 <rickzerr91@gmail.com>
2023-04-11 19:12:48 +0300

    less12 done

--- index.js
+++ index.js
@@ -71,3 +71,89 @@ guest1.email = "guest1@example.com";
 guest1.register();
 
 const databaseUsers = databaseObject.getUsers();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let head = {
+//     glasses: 1
+// };
+//
+// let table = {
+//     pen: 3,
+//     __proto__: head
+// };
+//
+// let bed = {
+//     sheet: 1,
+//     pillow: 2,
+//     __proto__: table
+// };
+//
+// let pockets = {
+//     money: 2000,
+//     __proto__: bed
+// };
+//
+// console.log(pockets.pen);
+// console.log(pockets.glasses);
+// console.log(head.glasses);
+
+
