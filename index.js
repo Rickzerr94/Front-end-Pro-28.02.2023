24701cb28b1dcce3380ecb718c053376102c0a5d
Rickzerr94 <rickzerr91@gmail.com>
2023-04-24 20:42:48 +0300

    less15 done

--- index.js
+++ index.js
@@ -1,94 +1,117 @@
-const strs = ["programming", "product", "procrastination"];
-
-function findPrefix(strs) {
-    if (!strs || strs.length === 0) {
-        return '';
+class Student {
+    constructor(firstName, lastName, year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.grades = [];
+        this.attendance = new Array(25).fill(undefined);
     }
 
-    const firstStr = strs[0];
-    let prefix = '';
-
-    for (let i = 0; i < firstStr.length; i++){
-        const char = firstStr[i];
-
-        for (let j = 1; j < strs.length; j++) {
-            if (i >= strs[j].length || strs[j][i] !== char) {
-                return prefix
-            }
-        }
-
-        prefix += char;
+    getAge() {
+        const currentYear = new Date().getFullYear();
+        return currentYear - this.year;
     }
 
-    return prefix;
-}
-
-const prefix = findPrefix(strs);
-console.log(prefix);
-
-////
-
-function twoDimToThreeDim(arr) {
-    let result = [];
-    let tempArr = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 0; j < arr[i].length; j++) {
-            if (tempArr.length < 3) {
-                tempArr.push(arr[i][j]);
-            } else {
-                result.push(tempArr);
-                tempArr = [arr[i][j]];
-            }
+    addGrade(grade) {
+        if (grade >= 0 && grade <= 100) {
+            this.grades.push(grade);
+        } else {
+            console.log("Оцінка повинна бути від 0 до 100");
         }
     }
 
-    if (tempArr.length > 0) {
-        result.push(tempArr);
+    getAverageGrade() {
+        const sum = this.grades.reduce((acc, val) => acc + val, 0);
+        return sum / this.grades.length || 0;
     }
 
-    return result;
-}
-
-let arr1 = [[1,2], [3,4], [5,6]];
-let result1 = twoDimToThreeDim(arr1);
-console.log(result1);
-
-let arr2 = [[1,2], [3,4], [5,6], [7]];
-let result2 = twoDimToThreeDim(arr2);
-console.log(result2);
-
-
-
-const arr3 = [[1, 2, 3], [4, 5, 6]];
-const flatArr = arr3.reduce((acc, val) => acc.concat(val), []);
-const result3 = [flatArr.slice(0, 2), flatArr.slice(2, 4), flatArr.slice(4)];
-console.log(result3);
-
-
-
-
-function convertArray(arr, length) {
-    let result = [];
-    let temp = [];
-
-    arr.forEach(subArr => {
-        subArr.forEach(element => {
-            if (temp.length === length) {
-                result.push(temp);
-                temp = [];
-            }
-            temp.push(element);
-        });
-    });
-
-    if (temp.length > 0) {
-        result.push(temp);
+    present() {
+        const index = this.attendance.indexOf(undefined);
+        if (index !== -1) {
+            this.attendance[index] = true;
+        } else {
+            console.log("Не можна відвідати більше 25 занять");
+        }
     }
 
-    return result;
+    absent() {
+        const index = this.attendance.indexOf(undefined);
+        if (index !== -1) {
+            this.attendance[index] = false;
+        } else {
+            console.log("Не можна пропустити більше 25 занять");
+        }
+    }
+
+    getAttendance() {
+        const presentCount = this.attendance.filter(a => a === true).length;
+        const absentCount = this.attendance.filter(a => a === false).length;
+        const totalClasses = presentCount + absentCount;
+        const attendanceRate = totalClasses > 0 ? presentCount / totalClasses : 0;
+        return {
+            presentCount,
+            absentCount,
+            totalClasses,
+            attendanceRate
+        };
+    }
+
+    summary() {
+        const averageGrade = this.getAverageGrade();
+        const attendanceRate = this.getAttendance().attendanceRate;
+        if (averageGrade > 90 && attendanceRate > 0.9) {
+            return "Молодець!";
+        } else if (averageGrade > 0 && attendanceRate > 0) {
+            return "Добре, але можна краще";
+        } else {
+            return "Редиска!";
+        }
+    }
 }
 
-const arr = [[1,2,3], [4,5,6]];
-console.log(convertArray(arr, 5));
+const student1 = new Student("Іван", "Іванов", 2000);
+const student2 = new Student("Марія", "Петрова", 2001);
 
+console.log(student1.getAge());
+console.log(student2.getAge());
+
+student1.addGrade(80);
+student1.addGrade(90);
+student1.addGrade(95);
+student2.addGrade(70);
+student2.addGrade(85);
+student2.addGrade(110);
+
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+student1.present();
+student1.absent();
+student1.present();
+student1.present();
+
+
+console.log(student1.getAverageGrade());
+console.log(student2.getAverageGrade());
+
+console.log(student1.getAttendance());
+console.log(student2.getAttendance());
