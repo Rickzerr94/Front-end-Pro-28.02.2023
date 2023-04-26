051c501e5beb1b37d68b895e6359a58599698251
Rickzerr94 <rickzerr91@gmail.com>
2023-04-26 20:41:32 +0300

    less16 done

--- index.js
+++ index.js
@@ -1,38 +1,24 @@
-class Hamburger {
-    static SIZE_SMALL = {price: 50, calories: 20};
-    static SIZE_BIG = {price: 100, calories: 40};
-    static STUFFING_CHEESE = {price: 10, calories: 20};
-    static STUFFING_SALAD = {price: 20, calories: 5};
-    static STUFFING_POTATO = {price: 15, calories: 10};
-    static TOPPING_MAYO = {price: 20, calories: 5};
-    static TOPPING_SPICE = {price: 15, calories: 0};
 
-    constructor(size, stuffing) {
-        this.size = size;
-        this.stuffing = stuffing;
-        this.toppings = [];
-    }
+const apiUrl = 'https://swapi.dev/api/people/';
 
-    addTopping(topping) {
-        this.toppings.push(topping);
-    }
+function getCharacters() {
+    return fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const allCharacters = data.results;
+            const filteredCharacters = allCharacters
+                .map(({name, mass, height, gender, films}) => ({
+                    name,
+                    mass,
+                    height,
+                    gender,
+                    films
+                }))
+                .filter(({films}) => films.length > 3);
+            console.log(filteredCharacters);
+        })
+        .catch(error => console.log(error));
 
-    calculatePrice() {
-        const toppingsPrice = this.toppings.reduce((acc, topping) => acc + topping.price, 0);
-        return this.size.price + this.stuffing.price + toppingsPrice;
-    }
-
-    calculateCalories() {
-        const toppingsCalories = this.toppings.reduce((acc, topping) => acc + topping.calories, 0);
-        return this.size.calories + this.stuffing.calories + toppingsCalories;
-    }
 }
 
-const hamburger = new Hamburger(Hamburger.SIZE_SMALL, Hamburger.STUFFING_CHEESE);
-hamburger.addTopping(Hamburger.TOPPING_MAYO);
-
-console.log("Calories: " + hamburger.calculateCalories());
-console.log("Price: " + hamburger.calculatePrice());
-
-hamburger.addTopping(Hamburger.TOPPING_SPICE);
-console.log("Price with sauce: " + hamburger.calculatePrice());
+console.log(getCharacters())
