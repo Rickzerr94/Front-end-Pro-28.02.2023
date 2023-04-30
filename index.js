23c5870ee4270d01909c11207707e6d4da120bc9
Rickzerr94 <rickzerr91@gmail.com>
2023-04-30 14:15:28 +0300

    less17 done

--- index.js
+++ index.js
@@ -1,24 +1,50 @@
+const filmsUrls = [
+    "https://swapi.dev/api/films/1/",
+    "https://swapi.dev/api/films/2/",
+    "https://swapi.dev/api/films/3/"
+];
 
-const apiUrl = 'https://swapi.dev/api/people/';
+const planetUrls = new Set();
 
-function getCharacters() {
-    return fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            const allCharacters = data.results;
-            const filteredCharacters = allCharacters
-                .map(({name, mass, height, gender, films}) => ({
-                    name,
-                    mass,
-                    height,
-                    gender,
-                    films
-                }))
-                .filter(({films}) => films.length > 3);
-            console.log(filteredCharacters);
-        })
-        .catch(error => console.log(error));
+const fetchPlanets = async (filmUrl) => {
+    const response = await fetch(filmUrl);
+    const film = await response.json();
+    const planetUrls = film.planets;
+    for (const planetUrl of planetUrls) {
+        await fetchPlanetName(planetUrl);
+    }
+};
 
-}
+const fetchPlanetName = async (planetUrl) => {
+    try {
+        const response = await fetch(planetUrl);
+        const planet = await response.json();
+        planetUrls.add(planet.name);
+    } catch (error) {
+        console.error(error);
+        console.log("Fetching starship...");
+        await fetchStarship();
+    }
+};
 
-console.log(getCharacters())
+const fetchStarship = async () => {
+    try {
+        const response = await fetch("https://swapi.dev/api/starships/9/");
+        const starship = await response.json();
+        console.log(`${starship.name} все уничтожил.`);
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+const fetchAllPlanets = async () => {
+    try {
+        const fetches = filmsUrls.map((filmUrl) => fetchPlanets(filmUrl));
+        await Promise.all(fetches);
+        console.log(Array.from(planetUrls));
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+console.log(fetchAllPlanets());
