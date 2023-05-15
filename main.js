7ea806f29ca9c03e7ee703a384c68a9ea1211108
Rickzerr94 <rickzerr91@gmail.com>
2023-05-15 18:11:22 +0300

    less22 done

--- main.js
+++ main.js
@@ -1,23 +1,40 @@
+let images = [
+    "./img/mic1.jpg",
+    "./img/mic2.jpg",
+    "./img/mic3.jpg",
+    "./img/mic4.jpg",
+    // Додайте шляхи до інших зображень
+];
 
-const categories = document.querySelectorAll('.category');
-const subcategories = document.querySelectorAll('.subcategory');
-const categorylist = document.querySelectorAll('.stufflist');
+let imageElement = document.getElementById("image");
+let prevBtn = document.getElementById("prevBtn");
+let nextBtn = document.getElementById("nextBtn");
+let currentImageIndex = 0;
 
+function showImage(index) {
+    imageElement.src = images[index];
+}
 
-categories.forEach((category, index) => {
-    category.addEventListener('click', () => {
-        subcategories.forEach((subcategory) => {
-            subcategory.classList.add('hidden');
-        });
-        subcategories[index].classList.remove('hidden');
-    });
+function updateButtons() {
+    prevBtn.disabled = currentImageIndex === 0;
+    nextBtn.disabled = currentImageIndex === images.length - 1;
+}
+
+prevBtn.addEventListener("click", function () {
+    if (currentImageIndex > 0) {
+        currentImageIndex--;
+        showImage(currentImageIndex);
+        updateButtons();
+    }
 });
 
-subcategories.forEach((subcategory, index) => {
-    subcategory.addEventListener('click', () => {
-        categorylist.forEach((stufflist) => {
-            stufflist.classList.add('hidden');
-        });
-        categorylist[index].classList.remove('hidden');
-    });
+nextBtn.addEventListener("click", function () {
+    if (currentImageIndex < images.length - 1) {
+        currentImageIndex++;
+        showImage(currentImageIndex);
+        updateButtons();
+    }
 });
+
+showImage(currentImageIndex);
+updateButtons();
