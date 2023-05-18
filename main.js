778b8b0cebec558daf059e054c3ff73bb7e92840
Rickzerr94 <rickzerr91@gmail.com>
2023-05-19 00:58:18 +0300

    less23 almost done

--- main.js
+++ main.js
@@ -27,7 +27,46 @@ subcategories.forEach((subcategory, index) => {
 
 buyButtons.forEach(button => {
     button.addEventListener('click', function() {
-        const productName = button.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
-        alert(`${productName} придбано`);
+        // const productName = button.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+        // alert(`${productName} придбано`);
+        openModal();
     });
 });
+
+function openModal() {
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('modalBackground').style.display = 'block';
+}
+
+function closeModal() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modalBackground').style.display = 'none';
+}
+
+function submitOrder(event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const city = document.getElementById('city').value;
+    const postOffice = document.getElementById('postOffice').value;
+    const paymentMethod = document.getElementById('paymentMethod').value;
+    const quantity = document.getElementById('quantity').value;
+    const comment = document.getElementById('comment').value;
+
+    if (!name || !city || !postOffice || !paymentMethod || !quantity) {
+        alert('Будь ласка, заповніть всі обов\'язкові поля');
+        return;
+    }
+
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('orderConfirmedModal').style.display = 'block';
+    setTimeout(function() {
+        closeOrderConfirmedModal();
+    }, 2000);
+}
+
+function closeOrderConfirmedModal() {
+    document.getElementById('orderConfirmedModal').style.display = 'none';
+    document.getElementById('modalBackground').style.display = 'none';
+}
+
+
