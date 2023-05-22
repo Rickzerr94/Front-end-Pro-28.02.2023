4850c6e5208c46b60c35b339458a19341b2697d6
Rickzerr94 <rickzerr91@gmail.com>
2023-05-22 12:40:18 +0300

    less24 almost done121

--- main.js
+++ main.js
@@ -25,13 +25,72 @@ subcategories.forEach((subcategory, index) => {
     });
 });
 
-buyButtons.forEach(button => {
-    button.addEventListener('click', function() {
-        // const productName = button.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
-        // alert(`${productName} придбано`);
-        openModal();
+// ======new code========
+
+// const buyButtons = document.querySelectorAll('.buy-button');
+const modal2 = document.getElementById('modal2');
+const cartItems = document.getElementById('cart-items');
+const closeButton = document.querySelector('.close');
+
+let cart = [];
+
+function addToCart(item) {
+    cart.push(item);
+}
+
+function updateCart() {
+    cartItems.innerHTML = '';
+
+    cart.forEach((item) => {
+        const li = document.createElement('li');
+        li.textContent = `${item.name} - Ціна: ${item.price} грн - ${item.date} ${item.time}`;
+        cartItems.appendChild(li);
     });
+}
+
+function handleBuyButtonClick(event) {
+    const item = {
+        name: event.target.parentElement.querySelector('h4').textContent,
+        price: event.target.parentElement.querySelector('p:last-child').textContent.split(': ')[1],
+        date: new Date().toLocaleDateString(),
+        time: new Date().toLocaleTimeString()
+    };
+
+    addToCart(item);
+}
+
+function handleCartButtonClick() {
+    updateCart();
+    modal2.style.display = 'block';
+}
+
+function handleCloseButtonClick() {
+    modal2.style.display = 'none';
+}
+
+buyButtons.forEach((button) => {
+    button.addEventListener('click', handleBuyButtonClick);
 });
+
+closeButton.addEventListener('click', handleCloseButtonClick);
+
+
+window.addEventListener('click', (event) => {
+    if (event.target === modal2) {
+        handleCloseButtonClick();
+    }
+});
+
+// ======new code end========
+
+
+// buyButtons.forEach(button => {
+//     button.addEventListener('click', function() {
+//         // const productName = button.previousElementSibling.previousElementSibling.previousElementSibling.previousElementSibling.textContent;
+//         // alert(`${productName} придбано`);
+//         openModal();
+//     });
+// });
 
 function openModal() {
     document.getElementById('modal').style.display = 'block';
@@ -61,7 +120,7 @@ function submitOrder(event) {
 }
 
 function openOrderConfirmedModal() {
-    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modal2').style.display = 'none';
     document.getElementById('orderConfirmedModal').style.display = 'block';
     // setTimeout(function () {
     //     closeOrderConfirmedModal();
